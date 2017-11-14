389063f329daef99f0fa8ea7d5ae099b85dbd807
David Artiss <david.artiss@automattic.com>
2017-11-14 11:08:50 +0000

    Add files via upload

--- wp-readme-parser.php
+++ wp-readme-parser.php
@@ -2,11 +2,12 @@
 /*
 Plugin Name: Plugin README Parser
 Plugin URI: https://wordpress.org/plugins/wp-readme-parser/
-Description: Display a WordPress plugin's README file in XHTML format, for embedding on a post or page.
-Version: 1.3.7
+Description: Show WordPress Plugin README in your Posts
+Version: 2.0
 Author: David Artiss
-Author URI: https://artiss.blog
+Author URI: http://www.artiss.co.uk
 Text Domain: wp-readme-parser
+Domain Path: /languages
 */
 
 /**
@@ -15,31 +16,18 @@ Text Domain: wp-readme-parser
 * Main code - include various functions
 *
 * @package	Artiss-README-Parser
-*/
-
-define( 'artiss_readme_parser_version', '1.3.7' );
-
-/**
-* Main Includes
-*
-* Include all the plugin's functions
-*
 * @since	1.2
 */
+
+define( 'artiss_readme_parser_version', '2.0' );
 
 $functions_dir = plugin_dir_path( __FILE__ ) . 'includes/';
 
 // Include all the various functions
 
-if ( is_admin() ) {
-
-    include_once( $functions_dir . 'admin-config.php' );			// Various admin. functions
-
-}
-
 include_once( $functions_dir . 'Michelf/MarkdownExtra.inc.php' );		// PHP Markdown Extra
 
-include_once( $functions_dir . 'functions.php' );					// Various functions
+include_once( $functions_dir . 'preprocess-file.php' );					// Various functions
 
-include_once( $functions_dir . 'generate-output.php' );				// Generate the output
+include_once( $functions_dir . 'shortcode.php' );				// Generate the output
 ?>
